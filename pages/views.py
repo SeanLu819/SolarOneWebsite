685ea649b67c2355c0c6adf4b6d0c4024e656366
SeanLu819 <sean.lu819@gmail.com>
2026-07-08 09:13:54 +0800

    Fix image paths: use static URLs instead of media for Vercel deployment

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib import messages
+from django.templatetags.static import static
 from pages.models import Product, Project, ContactMessage, SiteConfig
 
 
@@ -9,6 +10,30 @@ def home(request):
     site_config = SiteConfig.objects.first()
     if not site_config:
         site_config = SiteConfig.objects.create()
+
+    # Pre-compute static URLs for all ImageField references so they work
+    # on Vercel (ephemeral filesystem) where MEDIA files are unavailable.
+    for p in products:
+        if p.image:
+            p.image_url = static(p.image.name)
+        else:
+            p.image_url = ''
+
+    for proj in projects:
+        if proj.image:
+            proj.image_url = static(proj.image.name)
+        else:
+            proj.image_url = ''
+
+    if site_config.hero_background:
+        site_config.hero_bg_url = static(site_config.hero_background.name)
+    else:
+        site_config.hero_bg_url = static('images/hero-main.fw.png')
+
+    if site_config.logo:
+        site_config.logo_url = static(site_config.logo.name)
+    else:
+        site_config.logo_url = static('images/logo.png')
 
     if request.method == 'POST':
         name = request.POST.get('name', '')
