e4baee6671f485bb0a1228838a4d7fbd37e7ef80
SeanLu819 <sean.lu819@gmail.com>
2026-07-23 19:47:15 +0800

    i18n: fix i18n_patterns URL prefix - use prefix_default_language=False for Django 5.2 compat

--- solarone/urls.py
+++ solarone/urls.py
@@ -4,15 +4,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls.i18n import i18n_patterns
 
+# Use prefix_default_language=False so only non-default languages get a prefix
+# Default language (en) uses unprefixed URLs: /products/
+# Other languages use prefixed URLs: /fr/products/, /es/products/, etc.
 urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
+    prefix_default_language=False,
 )
-
-# Allow unprefixed URL to redirect to default language
-urlpatterns += [
-    path('', include('pages.urls')),
-]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
